b5f25b5e9503ca1aa71cda0369c0820c83999f5d
Mathilde-Garin <mathilde.garin@hotmail.fr>
2021-03-04 17:32:08 +0100

    1er commit template backoffice

---
+++ header.php
@@ -0,0 +1,119 @@
+<!-- Sidebar -->
+        <ul class="navbar-nav bg-dark-blue sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-text mx-3 ">
+                    <img src="./img/logo coloré et blanc.png">
+                </div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-dashboard.png">
+                    <span>Tableau de bord</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Artistes
+            </div>
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="php/artistes/artistes.php">
+                    <img src="./img/icone-music.png">
+                    <span>Liste des artistes</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-add.png">
+                    <span>Ajouter un artiste</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+           <!-- Heading -->
+           <div class="sidebar-heading">
+                Actualités
+            </div>
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-actu.png">
+                    <span>Liste des actualités</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-add.png">
+                    <span>Ajouter une actualité</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+           <!-- Heading -->
+           <div class="sidebar-heading">
+                Pages informatives
+            </div>
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-info.png">
+                    <span>Infos pratiques</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-info.png">
+                    <span>FAQ</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-carte.png">
+                    <span>Carte interactive</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <img src="./img/icone-user.png">
+                    <span>Utilisateurs</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
---
+++ navbar.php
@@ -0,0 +1,49 @@
+<!-- Topbar -->
+<nav class="navbar navbar-expand navbar-light bg-purple topbar mb-4 static-top shadow">
+
+    <!-- Sidebar Toggle (Topbar) -->
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+    </button>
+
+    <!-- Topbar Navbar -->
+    <ul class="navbar-nav ml-auto">
+
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-white small">Douglas McGee</span>
+            </a>
+        </li>
+        <div class="topbar-divider d-none d-sm-block"></div>
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-white"></i>Déconnexion</a>
+            </a>
+        </li>
+
+    </ul>
+
+</nav>
+<!-- End of Topbar -->
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Prêt à partir ?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Cliquez sur "Déconnecter" si vous souhaitez arrêter votre session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+                    <a class="btn btn-primary" href="login.html">Déconnecter</a>
+                </div>
+            </div>
+        </div>
+    </div>

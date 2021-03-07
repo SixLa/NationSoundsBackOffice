1194dfea435e3cacbe42814fcc913a72dfad48ef
SixLa <sixtine.lanord@gmail.com>
2021-03-07 12:08:00 +0100

    Lien avec BDD ok - Données apparaissent

--- map.php
+++ map.php
@@ -9,16 +9,19 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Back-office Nation Sounds</title>
+    <title>Nation Sounds</title>
 
-    <!-- Custom fonts for this template-->
+    <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- Custom styles for this template -->
     <link href="css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
@@ -28,11 +31,9 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-
         <?php
             include("header.php");
         ?>
-
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -41,62 +42,110 @@
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar /  -->
+                <!-- Topbar -->
                 <?php
                     include("navbar.php");
                 ?>
                 <!-- End of Topbar -->
 
-<!-- -------------------------------- Begin Page Content Dashboard -------------------------------- -->
+                <!-- Begin Page Content -->
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-white">Carte interactive</h1>
-                    </div>
+                    <h1 class="h3 mb-2 text-white">Tables</h1>
+                    <p class="mb-4 text-white">DataTables is a third party plugin that is used to generate the demo table below.
+                        For more information about DataTables, please visit the <a class="text-white" target="_blank"
+                            href="https://datatables.net">official DataTables documentation</a>.</p>
 
-                    <!-- 1st Row -->
-                    <div class="row">
-
-                        <!-- Content Column -->
-                        <div class="col-lg-12 mb-4">
-
-                            <!-- Artistes -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3 bg-dark-blue">
-                                    <h6 class="m-0 font-weight-bold text-white">Liste des points d'intérêts</h6>
-                                </div>
-                                <div class="card-body">
-                                    <a rel="nofollow" href="tables.php">
-                                        <div class="text-center">
-                                            <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                                src="img/undraw-music.svg" alt="">
-                                        </div>
-                                        <p class="text-center">> Consulter et mettre à jour la liste des artistes </p>
-                                    </a>
-                                </div>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>Airi Satou</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>33</td>
+                                            <td>2008/11/28</td>
+                                            <td class="d-flex justify-content-around">
+                                                <a href=""><i class="fas fa-eye btn-info btn.circle btn-sm" title="Voir"></i></a>
+                                                <a href=""><i class="fas fa-pencil-alt btn-primary btn.circle btn-sm" title="Modifier"></i></a>
+                                                <a href=""><i class="fas fa-trash btn-danger btn.circle btn-sm" title="Supprimer"></i></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Brielle Williamson</td>
+                                            <td>Integration Specialist</td>
+                                            <td>New York</td>
+                                            <td>61</td>
+                                            <td>2012/12/02</td>
+                                            <td>$372,000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Herrod Chandler</td>
+                                            <td>Sales Assistant</td>
+                                            <td>San Francisco</td>
+                                            <td>59</td>
+                                            <td>2012/08/06</td>
+                                            <td>$137,500</td>
+                                        </tr>  
+                                    </tbody>
+                                </table>
                             </div>
 
-                        </div>
+                            <!--Récupérer les infos dans la BDD et les afficher  -->
 
-                        <div class="col-lg-6 mb-4">
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=nationsounds;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+$reponse = $bdd->query('SELECT map_ID , map_nomlieu, map_filtre, map_longitude, map_latitude FROM info_map');
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>    
+    <strong>Jeu</strong> : <?php echo $donnees['map_ID']; ?><br />
+    Le possesseur de ce jeu est : <?php echo $donnees['map_nomlieu']; ?>, et il le vend à <?php echo $donnees['map_longitude']; ?> euros !<br />
+    Ce jeu fonctionne sur <?php echo $donnees['map_latitude']; ?>
+    <?php echo $donnees['map_ID']; ?> a laissé ces commentaires sur <?php echo $donnees['map_ID']; ?> : <em><?php echo $donnees['map_ID']; ?></em>
+   </p>
+<?php
+}
 
-                            <!-- Actualités -->
-                            
+$reponse->closeCursor(); // Termine le traitement de la requête
 
-                        </div>
-                        <?php $requete = "SELECT map_ID , map_filtre, map_longitude, map_latitude FROM info_map";
-
-    require("connection.php");
-    echo($requete)
 ?>
 
-                    <!-- 2nd Row -->
-                    <div class="row">
+<!-- Fin de la récupération -->
 
-                        
-                                        
-                        
+                        </div>
                     </div>
 
                 </div>
@@ -133,12 +182,18 @@
     <script src="js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="js/demo/datatables-demo.js"></script>
 
 </body>
 
 </html>
+
+
+
+
+
+
